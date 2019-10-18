d167c008afc70f58c6c8bb75a27e0a2c8ba88703
Ferran Cardoso <ferranc96@pc6-5.cancer.ucl.ac.uk>
2019-10-18 17:34:20 +0100

    Created new aux file. WIP

---
+++ Workflow/1-data_preprocess/panel_editing_functions.py
@@ -0,0 +1,92 @@
+import os, glob
+import pandas as pd
+import numpy as np
+import re
+
+
+#FCR 14/10/19: Automated column name editing with regex
+#Idea is to rename all columns and then filter non-relevant ones (less optimal,
+# easier and more compatible with writing new reduced file in the last step)
+
+#Renaming
+def rename_columns(df_file_cols):
+    reg_rename = re.compile("(__[a-z].*$|__\d.*$|_\(.*$|___.*$)")
+    df_file_cols_processed = []
+    df_file_cols_renamed = []
+    for i in df_file_cols:
+        try:
+            df_file_cols_processed.append(reg_rename.sub("",i))
+        except:
+            df_file_cols_processed.append(i)
+    #Second pass to remove trailing underscores
+    for i in df_file_cols_processed:
+        try:
+            df_file_cols_renamed.append(re.sub(r"_$","",i))
+        except:
+            df_file_cols_renamed.append(i)
+    # Keeping with Xiao's convention, rename Event # to Cell_Index
+    for n,i in enumerate(df_file_cols_renamed):
+        if i=="Event #":
+            df_file_cols_renamed[n] = "Cell_Index"
+    
+    return df_file_cols_renamed
+
+#Filtering
+def filter_columns(renamed_columns):
+    reg_filter = re.compile("^\d+[A-Za-z]+$")
+    filtered_columns = [] #Stores the columns that where deemed unnecessary
+    columns_to_keep = [] #Columns that the reduced file should have
+    for i in renamed_columns:
+        if reg_filter.search(i):
+            filtered_columns.append(i)
+        else:
+            columns_to_keep.append(i)
+    return columns_to_keep, filtered_columns
+
+
+
+#%%
+# Perform changes and save them to file
+cols = []
+for i in filelist:
+    file = f"./input/{i}"
+    df_file = pd.read_csv(file, sep = '\t')
+    shape_before = df_file.shape
+    df_file_cols = list(df_file.columns)
+    
+    #%% Perform renaming and filtering
+    renamed_columns = rename_columns(df_file_cols)
+    columns_to_keep, filtered_columns = filter_columns(renamed_columns)
+    df_file.columns = renamed_columns
+    f_reduced = df_file[columns_to_keep].iloc[:].copy()
+    print ("Removed the following columns: ", filtered_columns)
+    
+    #Store columns present in each of the input files
+    cols.append([x for x in f_reduced.columns if x[0].isdigit()])
+    # name = file.split(".")[0]
+    # print (i)
+    # print (file)
+    # print(name)
+    f_reduced.to_csv(f"./output/{i}", index = False, sep = '\t') 
+        # index = False to be compatible with Cytobank    
+    shape_after = f_reduced.shape
+    print(f"file: {i}\n\trows before: {shape_before[0]} - columns before: {shape_before[1]}\n\trows after: {shape_after[0]} - columns after: {shape_after[1]}\n")
+
+#%%
+#Add also the generation of a .csv file with the markers in the panel.
+#It should be ok to do it here b4 concatenation in the next step because if they are to be concatenaded they shpould already have the same panel of markers
+
+def write_panel_markers(cols):
+    all_markers = cols[0]
+    counter_marker = []
+    for i in all_markers:
+        counter_marker.append("N")
+    markers = pd.DataFrame(list(zip(all_markers, counter_marker)))
+    markers.to_csv(f"./output/panel_markers.csv", index=False, header=False)
+
+
+if not all(x==cols[0] for x in cols):
+    print ("Check your input files: The panels don't match!")
+else:
+    write_panel_markers(cols)
+
