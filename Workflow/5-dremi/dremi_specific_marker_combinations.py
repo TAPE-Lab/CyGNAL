4a1f6b5a4db4f25d66a5ce33951ba77b1605309f
Xiao Qin <xiaoqin@eduroam-int-dhcp-97-28-214.ucl.ac.uk>
2019-10-21 19:20:48 +0100

    To be continued on loop

--- Workflow/5-dremi/dremi_specific_marker_combinations.py
+++ Workflow/5-dremi/dremi_specific_marker_combinations.py
@@ -4,33 +4,32 @@
 # ms-python.python added
 import os
 try:
-	os.chdir(os.path.join(os.getcwd(), '../../../../../../var/folders/hw/fqkcybzs3gq_bksy6wfr534m0000gr/T'))
+	os.chdir(os.path.join(os.getcwd(), 'Workflow/5-dremi'))
 	print(os.getcwd())
 except:
 	pass
 
 #%%
-import fcsparser
+
 import pandas as pd
 import numpy as np
 import scprep
-import os
-from itertools import permutations 
-import holoviews as hv
-from IPython.core.interactiveshell import InteractiveShell
-#%matplotlib inline
+import sys
+from itertools import permutations
 
+import warnings
+warnings.filterwarnings('ignore')
 
 #%%
 # wide cells
+import holoviews as hv
+from IPython.core.interactiveshell import InteractiveShell
 hv.extension("bokeh", width=90)
 # display all output in each cell
 InteractiveShell.ast_node_interactivity = "all"
 
-
 #%%
 # parameter setup
-
 cofactor = 5
 k = 10
 n_bins = 20
@@ -42,48 +41,24 @@ return_drevi = False
 outliers_removal = False
 num_std_for_outliers = [3,4,5]
 
-
-#%%
-# set up working directory (dremi_dir)
-# files to be analysed are kept in a subfolder called 'all_for_dremi' in the current working folder (i.e. current_dir/all_for_dremi)
-
-current_dir = os.getcwd()
-dremi_dir = os.path.join(current_dir, "all_for_dremi")
-
-current_dir
-dremi_dir
-
-
-#%%
-# set up the list of markers between which dremi scores are going to be calculated (all marker-marker combinations)
-# "marker-for-dremi_fullset_29-in-total.txt" should be saved in the current working folder (current_dir)
-# change the name of the txt file if needed
-
-specific_markers = pd.read_csv("marker-for-dremi_fullset_28-in-total.txt", sep="\t")
-specific_markers
-
-
 #%%
 # create new folders to save the output of the script: plots, plots/drevi plots, and info
+if plot == True:
+    if os.path.isdir('./output/DREVI_plots') == False:
+        os.makedirs('./output/DREVI_plots')
 
-plots_dir = os.path.join(current_dir, "plots")
-# os.makedirs(plots_dir)
-
-drevi_plots_dir = os.path.join(plots_dir, "DREVI_plots")
-# os.makedirs(drevi_plots_dir)
-
-info_dir = os.path.join(current_dir, "info")
-if os.path.isdir(info_dir) == False:
-    os.makedirs(info_dir)
-
+if os.path.isdir('./output/info') == False:
+    os.makedirs('./output/info')
 
 #%%
 # create the list of fcs files to be analysed (all fcs files in the dremi_dir folder)
 
-dremi_files = [file for file in os.listdir(dremi_dir) if file.endswith(".fcs")]
-dremi_files
-len(dremi_files)
+dremi_files = [file for file in os.listdir('./input') if file.endswith(".txt")]
+if len(dremi_files) == 0:
+    sys.exit("Error: no txt files in the input folder")
 
+print('Sample files:')
+print('\n'.join([f for f in dremi_files]))
 
 #%%
 # find outliers for both marker_x and marker_y; create a dataframe with outliers removed
@@ -92,7 +67,7 @@ def z_score_outliers_removal(df, cutoff, marker_x, marker_y):
     num_outliers_total = 0
     num_outliers_x = 0
     num_outliers_y = 0
-        
+    
     df_outliers_x = df[(np.abs(df[marker_x]-df[marker_x].mean()) > (cutoff*df[marker_x].std()))]
     df_outliers_y = df[(np.abs(df[marker_y]-df[marker_y].mean()) > (cutoff*df[marker_y].std()))]
     num_outliers_x += df_outliers_x.shape[0]
@@ -104,58 +79,27 @@ def z_score_outliers_removal(df, cutoff, marker_x, marker_y):
 
     return(df_outliers_x, df_outliers_y, df_only_outliers_xy, df_wo_outliers, num_outliers_x, num_outliers_y, num_outliers_total)
 
-
-#%%
-import warnings
-warnings.filterwarnings('ignore')
-
-
 #%%
 # create a dataframe to store the dremi result
 df_info = pd.DataFrame()
 
-# calculate dremi and generate plot for each fcs file in the current_dir/all_for_dremi folder
-
+#################################################################################################
+# this block of code compiles the info of the sample, e.g. cell-type, cell-state, condition, etc.
+# but the most important information is the filename
+# the subset of the data can be done downstream when all the dremi scores have been calculated
 for f in dremi_files:
-    
-#################################################################################################
-# this block of code extracts the info of the sample, e.g. cell-type, cell-state, condition, etc.
-
-    name = f.split(".fcs")[0]
-
-#    if "_" in name:
-#        #name = f.split("_day_")[0]
-#        cell_type = name.split("_")[0]
-#        cell_state = name.split(f"{cell_type}_")[1]
-#        day = f.split(f"{cell_type}_{cell_state}_")[1].split(".fcs")[0]
-        
-#    else:
-#        cell_type = f.split("_day-")[0]
-#        cell_state = "All"
-#        day = f.split(f"{name}_")[1].split(".fcs")[0]
-
-#     cell_type = f.split("_")[1]
-#     cell_state = f.split(cell_type)[1][1:].split(".fcs")[0]
-#     day = f.split(f"{cell_type}_")[1].split("_new.fcs")[0]
-    
+    filename = f.split(".fcs")[0]
 #################################################################################################
 
-    # use fcsparser to read in the fcs file and perform arcsinh transformation (data vs. data_arc)
-
-    path = os.path.join(dremi_dir, f)
-    
-    meta, data = fcsparser.parse(path, reformat_meta=True)
-    data_arc = data.iloc[:,:].apply(lambda x: np.arcsinh(x/cofactor))
+    data = pd.read_csv(f'./input/{f}', sep = '\t')
+    markers = [m for m in list(data.columns) if m[0].isdigit()]
+    data_arc = data.loc[:, markers].apply(lambda x: np.arcsinh(x/cofactor))
     
     # generate the list of marker-marker pairs for dremi calculation 
-
-    marker_pairs = [comb for comb in list(permutations(list(specific_markers["marker"]), 2))]
+    marker_pairs = [comb for comb in list(permutations(list(markers["marker"]), 2))]
     
     # create folders to store drevi plots, arcsinh and no-arcsinh
-    # for dremi calculation, the raw data needs to be arcsinh-transformed
-    
-#     file_drevi_plots_dir = os.path.join(drevi_plots_dir, f"{cell_state}_{cell_type}")
-#     os.makedirs(file_drevi_plots_dir)
+    # for dremi calculation, the raw data needs to be arcsinh-transformed 
     
     for marker_x, marker_y in marker_pairs:
         
@@ -224,11 +168,11 @@ for f in dremi_files:
 #             df_info_dict["with_outliers_arcsinh_DREMI_score"] = dremi_with_outliers_arc
 #             df_info_dict["with_outliers_no-arcsinh_DREMI_score"] = dremi_with_outliers_no_arc
 #             ###### 
-             
+
             for cutoff in num_std_for_outliers:
                 colname_no_arc = f"wo_outliers_no-arcsinh_cutoff={cutoff}_std_DREMI_score"
                 colname_arc = f"wo_outliers_arcsinh_cutoff={cutoff}_std_DREMI_score"
-                   
+                
                 # call the function 'z_score_outliers_removal'
                 (data_no_arc_outliers_x, data_no_arc_outliers_y, data_no_arc_only_outliers_xy, data_no_arc_wo_outliers, data_no_arc_num_outliers_x, data_no_arc_num_outliers_y, data_no_arc_num_outliers_total) = z_score_outliers_removal(data, cutoff, marker_x, marker_y)
                 (data_arc_outliers_x, data_arc_outliers_y, data_arc_only_outliers_xy, data_arc_wo_outliers, data_arc_num_outliers_x, data_arc_num_outliers_y, data_arc_num_outliers_total) = z_score_outliers_removal(data_arc, cutoff, marker_x, marker_y)
@@ -261,4 +205,3 @@ for f in dremi_files:
 
 # save info in the dataframe df_info to a txt file
 df_info.to_csv(f"{info_dir}/dremi_info.txt", sep="\t")
-
