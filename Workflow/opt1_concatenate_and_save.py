8490cc5d62743822e436a1ec0d2bc19328326a61
Ferran Cardoso <44162441+FerranC96@users.noreply.github.com>
2019-11-07 10:57:58 +0000

    Minor script for processing days

--- Workflow/opt1_concatenate_and_save.py
+++ Workflow/opt1_concatenate_and_save.py
@@ -13,12 +13,25 @@ from aux_functions import *
 # IF WORKING WITH MULTIPLE FILES THEY SHOULD SHARE THE SAME MARKER
 # The concatenated file will be saved in the 'output/opt1_concatenation' folder
 # The user will need to change the name of the concatenate file and move it to the input folder for the next step
-folder_name = "./input/opt1_concatenation"
-filelist = [f for f in os.listdir(f"{folder_name}") if f.endswith(".txt")]
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CONFIG~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+folder_name = "opt1_concatenation"
+
+if os.path.isdir(f"./output/{folder_name}") == False:
+    os.makedirs(f"./output/{folder_name}")
+if os.path.isdir(f"./input/{folder_name}") == False:
+    os.makedirs(f"./input/{folder_name}")
+    sys.exit("ERROR: There is no input folder") 
+    
+input_dir = f"./input/{folder_name}"
+output_dir = f"./output/{folder_name}"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+filelist = [f for f in os.listdir(input_dir) if f.endswith(".txt")]
 #Check the files found in the directory:
 print ("Input files:")
 for i in filelist:
     print (i)
 
-concatenate_save(folder_name)
+concatenate_save(input_dir, output_dir)
