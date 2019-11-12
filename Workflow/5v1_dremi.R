f6b0aa484ed0e8e3aa1bef8a4e9d9c95175eaee3
Xiao Qin <qinxiao1990@gmail.com>
2019-11-12 16:41:34 +0000

    Heatmap for dremi and emd updated

--- Workflow/5v1_dremi.R
+++ Workflow/5v1_dremi.R
@@ -7,12 +7,15 @@ library(ggplot2)
 library(forcats)
 library(RColorBrewer)
 library(shiny)
+library(tidyverse)
 
 #Make sure no_norm is like this and marker_marker, not marker-marker -> check dremi script for populatiog the column itself, although i don't think that will pose a problem here in R
 
 dremi_info <- read_tsv(args)
 minx <- min(dremi_info$with_outliers_arcsinh_DREMI_score)
 maxx <- max(dremi_info$with_outliers_arcsinh_DREMI_score)
+
+print (unique(dremi_info$marker_x_marker_y))
 
 initial_dremi <- dremi_info %>% ggplot(aes(
                         x=file, y=factor(marker_x_marker_y,
@@ -27,16 +30,18 @@ ui <- fluidPage(
     # Application title
     titlePanel("DREMI scores heatmap"),
 
-    sidebarPanel(width=2,
+    sidebarPanel(width=3,
         sliderInput("range",
                     "Slider Range:",
                     step = 0.1,
                     min = floor(minx),
                     max = ceiling(maxx) +1,
                     value = c(minx, maxx)),
+        selectInput('in6', 'Select markers', unique(dremi_info$marker_x), multiple=TRUE, selectize=TRUE),
+        tags$hr(),
         downloadButton('foo', "Download plot as .pdf")
     ),
-    mainPanel(width=10,
+    mainPanel(width=9,
         plotlyOutput("trendPlot")
     )
 )
@@ -44,7 +49,22 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
+    output$out6 <- renderPrint(input$in6)
+    
     output$trendPlot <- renderPlotly({
+        if (!is.null(input$in6)) {
+            print (summary(dremi_info))
+            data_to_plot <- dremi_info %>% filter(str_detect(marker_x_marker_y, input$in6))
+            # data_to_plot <- dremi_info %>% filter(marker_x_marker_y %in% input$in6)
+            print (data_to_plot)
+        }
+        else{data_to_plot <- dremi_info}
+        
+        initial_dremi <- data_to_plot %>% ggplot(aes(
+            x=file, y=factor(marker_x_marker_y,
+                             levels=rev(unique(marker_x_marker_y))))) + 
+            geom_tile(aes(fill=with_outliers_arcsinh_DREMI_score)) +
+            labs(fill = "DREMI score")
         
         print(
         ggplotly(initial_dremi + scale_fill_gradient(
@@ -55,7 +75,8 @@ server <- function(input, output, session) {
                     axis.text.x = element_text(angle = 45, hjust = 1)) + 
             xlab("Condition") + ylab("Markers") +
             ggtitle("DREMI scores heatmap")
-        ) %>% layout(height = 1400, width = 1200))
+            ) %>% layout(height = 800) # %>% layout(height = 1400, width = 1200)
+        )
     })
     output$foo <- downloadHandler(
         filename = "dremi_heatmap.pdf",
