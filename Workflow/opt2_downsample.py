36bc50133180cfa8f8df6762c5118629cdfab54e
Xiao Qin <qinxiao1990@gmail.com>
2020-02-18 18:01:31 +0000

    updated downsample and reindex scripts

--- Workflow/opt2_downsample.py
+++ Workflow/opt2_downsample.py
@@ -22,16 +22,15 @@ print ("This script now downsamples multiple files on the input to the condition
 info_run =  input("Write downsampling info run (using no spaces!): ")
 
 concatenated_df = concatenate_fcs(input_dir)[0]
-
 downsampled_conc_df = downsample_data(concatenated_df, info_run, output_dir)
 
-#Since after downsampling file_origin is also an index, drop index from dataframe
-for name, group in downsampled_conc_df.reset_index(drop=True).groupby("file_origin"):
+# Since after downsampling file_origin is also an index, drop index from dataframe
+for name, group in downsampled_conc_df.reset_index(drop=True).groupby("file_identifier"):
     print (name)
     print (group)
-    group.to_csv(f"{output_dir}/{name}_downsample_{info_run}.txt",
-                    index = False, sep = '\t')
-
+    group.reset_index()
+    # group['post-downsample_cell-index'] = group.index
+    group.to_csv(f"{output_dir}/{info_run}/{name}_downsample_{info_run}.txt", index = True, sep = '\t')
 
 #Cell state sepration -> Interactive queastion, defaul file origin
 # #Divide concatenated into separate files
