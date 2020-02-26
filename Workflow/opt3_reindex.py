cb5433c00fa90d277a6c63fde25ba72d29602e07
Xiao Qin <qinxiao1990@gmail.com>
2020-02-26 11:33:02 +0000

    Fixed folder error in opt3_reindex.py

--- Workflow/opt3_reindex.py
+++ Workflow/opt3_reindex.py
@@ -1,8 +1,17 @@
 #! python3
 import os
 import pandas as pd
+import sys
 
 filelist = [f for f in os.listdir(f"./input/opt3_reindex") if f.endswith(".csv") or f.endswith(".txt")]
+
+folder_name = 'opt3_reindex'
+
+if os.path.isdir(f"./output/{folder_name}") == False:
+    os.makedirs(f"./output/{folder_name}")
+if os.path.isdir(f"./input/{folder_name}") == False:
+    os.makedirs(f"./input/{folder_name}")
+    sys.exit("ERROR: There is no input folder") 
 
 for f in filelist:
     df = pd.read_csv(f'./input/opt3_reindex/{f}', sep = '\t', index_col = 0)
