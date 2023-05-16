4244e322688d97ff7f2da527ca547d60493ad25a
FerranC96 <ferricaro@hotmail.com>
2023-05-16 12:30:39 +0100

    Added dosctrings to aux functions. Todo pd.concat

--- code/aux/aux3_emd.py
+++ code/aux/aux3_emd.py
@@ -6,9 +6,22 @@ import pandas as pd
 import scprep
 
 # Function to calculate EMD
-
+#The function calculates the EMD between two distributions for each marker and stores the results in a DataFrame. The EMD measures the minimum amount of work required to transform one distribution into another, considering the distances between individual data points.
 def calculate_emd(marker_list, emd_infodict, compare_from, compare_to,
                     emd_df):
+    """
+    Calculates Earth Mover's Distance (EMD) between two distributions for each marker in the given marker list.
+
+    Args:
+        marker_list (list): List of markers.
+        emd_infodict (dict): EMD information dictionary.
+        compare_from (DataFrame): Data for comparison from.
+        compare_to (DataFrame): Data for comparison to.
+        emd_df (DataFrame): DataFrame to store EMD results.
+
+    Returns:
+        DataFrame: Updated DataFrame with EMD results.
+    """
     deprecated_string = "no_norm" #No normalisation implemented. Deprecate
     for marker in marker_list:
         emd_infodict["marker"] = marker
